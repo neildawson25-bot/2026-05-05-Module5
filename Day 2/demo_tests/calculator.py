a6e8f9ae40d84fbdd6b4334900bd12a9bb180c9c
Neil Dawson <neildawson25@gmail.com>
2026-05-06 12:03:13 +0100

    day 2 tests demo start

---
+++ Day 2/demo_tests/calculator.py
@@ -0,0 +1,20 @@
+class Calculator:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+        
+    def get_sum(self):
+        return self.a + self.b
+    
+    def get_subrtraction(self):
+        return self.a - self.b
+    
+    def get_multiplication(self):
+        return self.a * self.b
+
+    def get_division(self):
+        return self.a / self.b
+
+
+        myCalc = Calculator(a=3,b=5)
+        print(myCalc.get_division())
---
+++ Day 2/demo_tests/scientific_calc.py
@@ -0,0 +1,5 @@
+from calculator import Calculator
+
+myCalcV2 = Calculator(2,4)
+
+print(myCalcV2.get_multiplication())
